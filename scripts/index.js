b996c3cdb7a046cb70311234cfe1e748be165cdf
srehan1 <syed.rehan@shoppertrak.com>
2022-09-11 13:27:40 +0530

    Logic for game play

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+function rollDice() {
+  var dice = Math.floor(Math.random() * 6 + 1);
+  return dice;
+}
+
+var randomNumber1 = rollDice();
+var randomNumber2 = rollDice();
+
+// Selecting dice images
+var diceImg1 = document.querySelector(".img1");
+var diceImg2 = document.querySelector(".img2");
+// Heading
+var heading = document.querySelector(".container h1");
+
+// Change dice images
+diceImg1.setAttribute("src", `images/dice${randomNumber1}.png`);
+diceImg2.setAttribute("src", `images/dice${randomNumber2}.png`);
+
+// Decide who won the game
+if (randomNumber1 > randomNumber2) {
+  heading.textContent = "Player 1 wins 🚩";
+} else if (randomNumber1 < randomNumber2) {
+  heading.textContent = "Player 2 wins 🚩";
+} else if (randomNumber1 === randomNumber2) {
+  heading.textContent = "Draw 😶";
+}
